aebdaa1b22abedf8fc6c79696a1e997b117674e7
Mohamed Wagih <mohamed0wagih@gmail.com>
2022-07-03 21:17:01 +0200

    :recycle: refactor navbar and footer width

--- src/layout/Footer/Footer.jsx
+++ src/layout/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import { Box, Center, Text } from '@chakra-ui/react';
 
 function Footer() {
   return (
-    <Box w="100%" p={4}>
+    <Box p={4}>
       <Center>
         <Text fontSize="xs">Guestbook</Text>
       </Center>
--- src/layout/Navbar/Navbar.jsx
+++ src/layout/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import { Box, Heading } from '@chakra-ui/react';
 
 function Navbar() {
   return (
-    <Box bg="gray" w="100%" p={4} color="white">
+    <Box bg="gray" p={4} color="white">
       <Heading m={0} as="h4" size="md">
         Guestbook
       </Heading>
