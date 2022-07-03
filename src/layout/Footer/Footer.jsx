ee0e5beb006b7e9871e1b47514a0c73513261a59
Mohamed Wagih <mohamed0wagih@gmail.com>
2022-07-03 21:07:53 +0200

    :sparkles: Footer

---
+++ src/layout/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import { Box, Center, Text } from '@chakra-ui/react';
+
+function Footer() {
+  return (
+    <Box w="100%" p={4}>
+      <Center>
+        <Text fontSize="xs">Guestbook</Text>
+      </Center>
+    </Box>
+  );
+}
+
+export default Footer;
