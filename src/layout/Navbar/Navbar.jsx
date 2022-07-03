3d58ce796003995de802a40bb08e736b0fda876a
Mohamed Wagih <mohamed0wagih@gmail.com>
2022-07-04 00:46:08 +0200

    :sparkles: logout

--- src/layout/Navbar/Navbar.jsx
+++ src/layout/Navbar/Navbar.jsx
@@ -1,11 +1,48 @@
-import { Box, Heading } from '@chakra-ui/react';
+import { Avatar, Box, Center, Flex, Heading, IconButton, Spacer, Stack, Text } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+import { FiLogOut } from 'react-icons/fi';
+import { useNavigate } from 'react-router-dom';
 
 function Navbar() {
+  const navigate = useNavigate();
+
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem('guestbook_user'));
+    if (user) setUser(user);
+    else {
+      setUser(null);
+    }
+  }, [localStorage.getItem('guestbook_user')]);
+
+  const logout = () => {
+    localStorage.removeItem('guestbook_user');
+    navigate('/');
+  };
+
   return (
     <Box bg="gray" p={4} color="white">
-      <Heading m={0} as="h4" size="md">
-        Guestbook
-      </Heading>
+      <Flex>
+        <Heading m={0} as="h4" size="md">
+          Guestbook
+        </Heading>
+        <Spacer />
+        {user && (
+          <>
+            <Stack direction="row" gap={1}>
+              <Avatar size="sm" name={user.name} />
+
+              <Center>
+                <Text fontSize="lg">{user.name}</Text>
+              </Center>
+              <Center>
+                <IconButton colorScheme="red" rightIcon={<FiLogOut />} onClick={logout} size="sm" />
+              </Center>
+            </Stack>
+          </>
+        )}
+      </Flex>
     </Box>
   );
 }
--- src/screens/Messages/Messages.jsx
+++ src/screens/Messages/Messages.jsx
@@ -1,15 +1,19 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import CreateMessage from './components/CreateMessage';
 import Message from './components/Message';
 
 const apiEndpoint = process.env.REACT_APP_API_ENDPOINT;
 
 function Messages() {
+  const navigate = useNavigate();
+
   const [messages, setMessages] = useState([]);
 
   const getMessages = async () => {
     const user = JSON.parse(localStorage.getItem('guestbook_user'));
+    if (!user) navigate('/');
     console.log('🚀 ~ Messages ~ user', user);
     const res = await axios.get(`${apiEndpoint}/messages`, {
       headers: { 'x-auth-token': user.token },
