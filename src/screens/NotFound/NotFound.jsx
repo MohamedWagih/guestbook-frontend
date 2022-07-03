f7280036409495fb0d5908542a8de013f18dfc3e
Mohamed Wagih <mohamed0wagih@gmail.com>
2022-07-03 21:17:28 +0200

    :sparkles: NotFound screen

---
+++ src/screens/NotFound/NotFound.jsx
@@ -0,0 +1,11 @@
+import { Center, Heading } from '@chakra-ui/react';
+
+function NotFound() {
+  return (
+    <Center color="Black">
+      <Heading>There is nothing here!</Heading>
+    </Center>
+  );
+}
+
+export default NotFound;
