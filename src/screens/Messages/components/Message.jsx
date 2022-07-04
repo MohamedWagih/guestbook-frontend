d691eceeda3f5a4063f7fef4014484a3a1fdc32f
Mohamed Wagih <mohamed0wagih@gmail.com>
2022-07-04 21:30:44 +0200

    :sparkles: message delete

--- src/screens/Messages/components/Message.jsx
+++ src/screens/Messages/components/Message.jsx
@@ -1,23 +1,68 @@
-import { Avatar, Box, Center, Flex, Stack, Text } from '@chakra-ui/react';
+import { Avatar, Box, Center, Flex, IconButton, Text } from '@chakra-ui/react';
+import axios from 'axios';
 import PropTypes from 'prop-types';
+import { useEffect, useState } from 'react';
+import { FiEdit3 } from 'react-icons/fi';
+import { MdDeleteForever } from 'react-icons/md';
+import { useNavigate } from 'react-router-dom';
 import CreateReply from './CreateReply';
 import Reply from './Reply';
+
+const apiEndpoint = process.env.REACT_APP_API_ENDPOINT;
+
 function Message({ message }) {
+  const navigate = useNavigate();
+
+  const [user, setUser] = useState(null);
+  const [isDeleting, setIsDeleting] = useState(false);
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem('guestbook_user'));
+    setUser(user);
+  }, []);
+
+  const deleteMessage = async () => {
+    try {
+      setIsDeleting(true);
+      await axios.delete(`${apiEndpoint}/messages/${message._id}`, {
+        headers: { 'x-auth-token': user.token },
+      });
+      navigate('/temp');
+      navigate(-1);
+    } catch (error) {
+      console.log('🚀 ~ deleteMessage ~ error', error);
+    } finally {
+      setIsDeleting(false);
+    }
+  };
+
   return (
     <Box boxShadow="md" p="6" rounded="md" m="4" bg="gray.300">
       <Flex direction="row" gap={4}>
         <Avatar name={message.author.name} />
         <Flex flexDir="column" flex="1">
-          <Stack direction="row">
+          <Flex flexDirection="row" justifyContent="space-between">
             <Center>
               <Text fontSize="lg">{message.author.name}</Text>
             </Center>
             <Center>
               <Text fontSize="xs">{message.date}</Text>
             </Center>
-          </Stack>
+            {user && user.id === message.author._id && (
+              <Center>
+                <IconButton icon={<FiEdit3 />} />
+                <IconButton
+                  onClick={deleteMessage}
+                  marginLeft={1}
+                  colorScheme="red"
+                  icon={<MdDeleteForever />}
+                  isLoading={isDeleting}
+                />
+              </Center>
+            )}
+          </Flex>
           <Text>{message.content}</Text>
-          <Box marginLeft={4} bg="gray.500">
+          <Box bg="gray.500">
             <CreateReply messageId={message._id} />
             {message.replies.map((reply) => (
               <Reply key={reply._id} reply={reply} />
